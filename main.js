5b3063d255186a5e2e4bded9e1125da04a0cecb0
smmya <69052436+smmya@users.noreply.github.com>
2022-05-20 23:33:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -36,9 +36,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'https://mplay.top/1.jpg': 'center',
-							'https://mplay.top/2.jpg': 'center',
-							'https://mplay.top/5.jpg': 'center'
+							'https://cdn.jsdelivr.net/gh/smmya/index@gh-pages/1.jpg': 'center',
+							'https://cdn.jsdelivr.net/gh/smmya/index@gh-pages/2.jpg': 'center',
+							'https://cdn.jsdelivr.net/gh/smmya/index@gh-pages/5.jpg': 'center'
 						},
 
 					// Delay.
